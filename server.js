f97dff690ecee3086bfdcdc5f4c240146b4d68c7
devonbahary <devonbahary@gmail.com>
2018-03-15 10:23:49 -0700

    Add project page

--- server.js
+++ server.js
@@ -22,9 +22,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -49,9 +50,15 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-      pageTitle: 'About Page'
+        pageTitle: 'About Page'
     });
 });
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    });
+})
 
 app.get('/bad', (req, res) => {
     res.send({
